ea5bfff868d50e0a8b11e8adb0ebb865e71d43d0
Michael Lee <melon0307@hotmail.com>
2022-07-04 11:30:02 +0800

    7.4 第一支Ajax程式

---
+++ prjMSIT141AjaxSite/Controllers/ApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prjMSIT141AjaxSite.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("AJax, 你好","text/plain",System.Text.Encoding.UTF8);
+        }
+    }
+}
